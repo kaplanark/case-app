759c65ef78f1969e09e44dabea2a4628fae9207b
kaplanark <kaplanarkan@gmail.com>
2024-07-12 00:12:47 +0300

    add: added home screen

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { NavigationAction } from '@react-navigation/native';
+
+export function HomeScreen({ navigation }:{navigation:NavigationAction}):React.ReactElement {
+  return (
+    <View>
+      
+    </View>
+  );
+}
