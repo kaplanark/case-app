d16d33a05b803e7a50d06b211d5bf2d34d5020cc
kaplanark <kaplanarkan@gmail.com>
2024-07-12 16:25:41 +0300

    create: data and filter types

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,24 @@
 export type SortOptions = "new" | "old" | "rate" | "random";
+
+export interface GlobalResponse {
+    status: number;
+    message: string;
+    data: ResponseData;
+}
+
+export interface Entry {
+    title: string,
+    description: string,
+    programType: string,
+    images: {
+        PosterArt: {
+            url: string
+        }
+    },
+    releaseYear: number,
+}
+
+export interface ResponseData {
+    total: number,
+    entries: Entry[]
+}
